acf2c370e61190858fd33d33280ad2c128d6a000
rajeev singh <rajeevkri@gmail.com>
2017-08-01 13:54:53 +0530

    added steps

--- src/com/test/hackerrank/array/AlgorithmicCrush.java
+++ src/com/test/hackerrank/array/AlgorithmicCrush.java
@@ -3,6 +3,17 @@ package com.test.hackerrank.array;
 import java.util.Scanner;
 
 public class AlgorithmicCrush {
+
+	/*
+	 * Instead of storing the actual values in the array, you store the
+	 * difference between the current element and the previous element. So you
+	 * add sum to a[p] showing that a[p] is greater than its previous element by
+	 * sum. You subtract sum from a[q+1] to show that a[q+1] is less than a[q]
+	 * by sum (since a[q] was the last element that was added to sum). By the
+	 * end of all this, you have an array that shows the difference between
+	 * every successive element. By adding all the positive differences, you get
+	 * the value of the maximum element
+	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		long n = in.nextLong();
@@ -12,8 +23,9 @@ public class AlgorithmicCrush {
 			int a = in.nextInt();
 			int b = in.nextInt();
 			int k = in.nextInt();
-			diffArr[a-1] +=k;
-			if(b<n+1) diffArr[b] -=k;
+			diffArr[a - 1] += k;
+			if (b < n + 1)
+				diffArr[b] -= k;
 		}
 		long max = diffArr[0];
 		long currentSum = diffArr[0];
@@ -23,6 +35,6 @@ public class AlgorithmicCrush {
 				max = currentSum;
 			}
 		}
-		System.out.println( max);
+		System.out.println(max);
 	}
 }
