9b796c6256964978aa77007cfa45b3b05bab1115
rajeev singh <rajeevkri@gmail.com>
2017-08-04 15:16:36 +0530

    Find four element added to some value, election winner, triangle or not, naxXor, maxMoney, firstRepeated word

--- src/com/test/common/Operations.java
+++ src/com/test/common/Operations.java
@@ -1,12 +1,16 @@
 package com.test.common;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
 import java.util.TreeMap;
 
@@ -280,6 +284,9 @@ public class Operations {
 		stack.push(2);
 		stack.addAll(1, 3);
 		stack.peek();
+
+		Scanner in = new Scanner(System.in);
+
 	}
 
 	static Map<String, Integer> datesMap = new HashMap<>();
@@ -329,58 +336,269 @@ public class Operations {
 	}
 
 	// Function to check if two given pairs have any common element or not
-	boolean noCommon(PairSum a, PairSum b)
-	{
-	    if (a.first == b.first || a.first == b.sec ||
-	            a.sec == b.first || a.sec == b.sec)
-	        return false;
-	    return true;
+	boolean noCommon(PairSum a, PairSum b) {
+		if (a.first == b.first || a.first == b.sec || a.sec == b.first || a.sec == b.sec)
+			return false;
+		return true;
 	}
-	 
-	 
+
 	// The function finds four elements with given sum X
-	void findFourElements (int arr[], int n, int X)
-	{
-	    int i, j;
-	 
-	    // Create an auxiliary array to store all pair sums
-	    int size = (n*(n-1))/2;
-	    PairSum[] aux = new PairSum[size];
-	 
-	    /* Generate all possible pairs from A[] and store sums
-	       of all possible pairs in aux[] */
-	    int k = 0;
-	    for (i = 0; i < n-1; i++)
-	    {
-	        for (j = i+1; j < n; j++)
-	        {
-	            aux[k].sum = arr[i] + arr[j];
-	            aux[k].first = i;
-	            aux[k].sec = j;
-	            k++;
-	        }
-	    }
-	 
-	    // Sort the aux[] array using library function for sorting
-	    Arrays.sort(aux);
-	 
-	    // Now start two index variables from two corners of array
-	    // and move them toward each other.
-	    i = 0;
-	    j = size-1;
-	    while (i < size && j >=0 )
-	    {
-	        if ((aux[i].sum + aux[j].sum == X) && noCommon(aux[i], aux[j]))
-	        {
-	            System.out.printf("%d, %d, %d, %d\n", arr[aux[i].first], arr[aux[i].sec],
-	                                     arr[aux[j].first], arr[aux[j].sec]);
-	            return;
-	        }
-	        else if (aux[i].sum + aux[j].sum < X)
-	            i++;
-	        else
-	            j--;
-	    }
+	void findFourElements(int arr[], int n, int X) {
+		int i, j;
+
+		// Create an auxiliary array to store all pair sums
+		int size = (n * (n - 1)) / 2;
+		PairSum[] aux = new PairSum[size];
+
+		/*
+		 * Generate all possible pairs from A[] and store sums of all possible
+		 * pairs in aux[]
+		 */
+		int k = 0;
+		for (i = 0; i < n - 1; i++) {
+			for (j = i + 1; j < n; j++) {
+				aux[k].sum = arr[i] + arr[j];
+				aux[k].first = i;
+				aux[k].sec = j;
+				k++;
+			}
+		}
+
+		// Sort the aux[] array using library function for sorting
+		Arrays.sort(aux);
+
+		// Now start two index variables from two corners of array
+		// and move them toward each other.
+		i = 0;
+		j = size - 1;
+		while (i < size && j >= 0) {
+			if ((aux[i].sum + aux[j].sum == X) && noCommon(aux[i], aux[j])) {
+				System.out.printf("%d, %d, %d, %d\n", arr[aux[i].first], arr[aux[i].sec], arr[aux[j].first],
+						arr[aux[j].sec]);
+				return;
+			} else if (aux[i].sum + aux[j].sum < X)
+				i++;
+			else
+				j--;
+		}
 	}
-	 
+
+	static void customSort(int[] arr) {
+		int n = arr.length;
+		Map<ElementFrequency, Integer> map = new TreeMap<>();
+		for (int i = 0; i < n; i++) {
+			int k = arr[i];
+			ElementFrequency elementFrequency = new Operations().new ElementFrequency();
+			elementFrequency.element = k;
+			Integer frequency = map.get(elementFrequency);
+			if (frequency != null) {
+				frequency++;
+			} else {
+				frequency = 1;
+			}
+			elementFrequency.frequency = frequency;
+			map.put(elementFrequency, frequency);
+		}
+
+		for (Map.Entry<ElementFrequency, Integer> entry : map.entrySet()) {
+			Integer value = entry.getValue();
+			ElementFrequency key = entry.getKey();
+			int k = key.element;
+			for (int i = 0; i < value; i++) {
+				System.out.println(k);
+			}
+		}
+	}
+
+	class ElementFrequency implements Comparable<ElementFrequency> {
+		int element;
+		int frequency;
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + element;
+			result = prime * result + frequency;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ElementFrequency other = (ElementFrequency) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (element != other.element)
+				return false;
+			if (frequency != other.frequency)
+				return false;
+			return true;
+		}
+
+		private Operations getOuterType() {
+			return Operations.this;
+		}
+
+		@Override
+		public int compareTo(ElementFrequency o) {
+			int frqDiff = frequency - o.frequency;
+			int numDiff = element - o.element;
+			if (frqDiff == 0 && numDiff == 0) {
+				return 0;
+			} else if (frqDiff > 0)
+				return 1;
+			else if (numDiff > 0) {
+				return 1;
+			}
+			return -1;
+		}
+
+	}
+
+	static String firstRepeatedWord(String s) {
+
+		Map<String, Boolean> map = new HashMap<>();
+		String[] arr = s.split("[^\\w']+");
+		String repeatedWord = "";
+		for (String a : arr) {
+			if (map.get(a) != null) {
+				repeatedWord = a;
+				break;
+			}
+			map.put(a, Boolean.TRUE);
+		}
+
+		return repeatedWord;
+	}
+
+	static int maxMoney(int n, long k) {
+		// int sumToReduce = 1;
+		long currentSum = 0;
+		for (int i = 1; i <= n; i++) {
+			currentSum += i;
+			if (currentSum == k) {
+
+				currentSum = currentSum - 1;
+				// sumToReduce++;
+				continue;
+			}
+		}
+
+		if (currentSum > Integer.MAX_VALUE) {
+			Double d = Math.pow(10, 9) + Double.valueOf(7);
+			currentSum = currentSum % d.longValue();
+		}
+		return Long.valueOf(currentSum).intValue();
+	}
+
+	//Hackerland election
+	static String electionWinner(String[] votes) {
+        Map<String, Integer> map = new HashMap<>();
+        int max = 0;
+        for (String vote: votes) {
+            if (map.get(vote) == null) map.put(vote, 1);
+            else {
+                int count = map.get(vote)+1;
+                map.put(vote, count);
+                max = Math.max(max, count);
+            }
+            //map.put(vote, map.getOrDefault(vote, 0)+1);
+        }
+        List<String> res = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            if (entry.getValue() == max) res.add(entry.getKey());
+        }
+        Collections.sort(res);
+        return res.get(res.size()-1);
+    }
+	
+	//The Bit Game
+	static int maxXor(int l, int r, int k) {
+        if (l == r) return 0;
+        if (l > r) return maxXor(r, l, k);
+        int max = 0;
+        for (int i = l; i <= r; i++) {
+            for (int j = l; j <= r; j++) {
+                if ((i ^ j) > k) continue;
+                max = Math.max(max, i ^ j);
+            }
+        }
+        return max;
+    } 
+	
+	
+	int solution(int n) {
+		char[] d = Integer.toBinaryString(n).toCharArray(); // convert to binary
+															// and split by
+															// letters (example:
+															// d = ['1', '0',
+															// '0', ...])
+
+		// outer is a label that helps us jump back to this loop when we fail
+		// (thus quitting the inner loop) (better than the var ok)
+		outer: for (char p = 1; p <= d.length / 2; p++) { // for p = 0 and p is
+															// less than half (P
+															// ≤ Q / 2)
+			for (char i = 0; i < d.length - p; i++) {
+				if (d[i] != d[i + p]) // if there is two letters that are
+										// separated by the p and not equal then
+										// ignore the rest and jump back to the
+										// outer loop (outer:)
+					continue outer;
+			}
+			// if no then this must be the period (return it)
+			return p;
+		}
+		// if all the possible periods failed, return -1
+		return -1;
+	}
+
+	int solution1(int n) {
+		int[] d = new int[30];
+		int l = 0;
+		int p;
+		while (n > 0) {
+			d[l] = n % 2;
+			n /= 2;
+			l++;
+		}
+		outer: for (p = 1; p < 1 + l; ++p) {
+			int i;
+			boolean ok = true;
+			for (i = 0; i < l - p; ++i) {
+				if (d[i] != d[i + p]) {
+					ok = false;
+					continue outer;
+				}
+			}
+			if (ok) {
+				return p;
+			}
+		}
+		return -1;
+	}
+
+	static String[] triangleOrNot(int[] a, int[] b, int[] c) {
+
+		int n = a.length;
+		String[] arr = new String[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = isTrianglePossible(a[i], b[i], c[i]) ? "Yes" : "No";
+		}
+		return arr;
+	}
+
+	static boolean isTrianglePossible(int a, int b, int c) {
+		if ((a + b) > c && (a + c) > b && (b + c) > a) {
+			return true;
+		}
+		return false;
+	}
 }
