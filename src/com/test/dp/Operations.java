ec7dd217d49a0865cabeb75107e94f9316ad37b7
rajeev singh <rajeevkri@gmail.com>
2017-08-07 13:28:28 +0530

    longest increasing subsequence in a array

--- src/com/test/dp/Operations.java
+++ src/com/test/dp/Operations.java
@@ -149,4 +149,53 @@ public class Operations {
 		return max;
 	}
 
+	public int longestIncreasingSubSequence(int arr[]){
+        int T[] = new int[arr.length];
+        int R[] = new int[arr.length];
+        for(int i=0; i < R.length ; i++) {
+            R[i] = -1;
+        }
+        T[0] = 0;
+        int len = 0;
+        for(int i=1; i < arr.length; i++){
+            if(arr[T[0]] > arr[i]){ //if input[i] is less than 0th value of T then replace it there.
+                T[0] = i;
+            }else if(arr[T[len]] < arr[i]){ //if input[i] is greater than last value of T then append it in T
+                len++;
+                T[len] = i;
+                R[T[len]] = T[len-1];
+            }else{ //do a binary search to find ceiling of input[i] and put it there.
+                int index = ceilIndex(arr, T, len,arr[i]);
+                T[index] = i;
+                R[T[index]] = T[index-1];
+            }
+        }
+
+        //this prints increasing subsequence in reverse order.
+        System.out.print("Longest increasing subsequence ");
+        int index = T[len];
+        while(index != -1) {
+            System.out.print(arr[index] + " ");
+            index = R[index];
+        }
+
+        System.out.println();
+        return len+1;
+    }
+	private int ceilIndex(int input[], int T[], int end, int s){
+        int start = 0;
+        int middle;
+        int len = end;
+        while(start <= end){
+            middle = (start + end)/2;
+            if(middle < len && input[T[middle]] < s && s <= input[T[middle+1]]){
+                return middle+1;
+            }else if(input[T[middle]] < s){
+                start = middle+1;
+            }else{
+                end = middle-1;
+            }
+        }
+        return -1;
+    }
 }
