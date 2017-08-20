7c62deeb66df7f4bcd6b750a9732b756c7b61777
Rajeev Krishna Singh <rajeevkri@gmail.com>
2017-08-21 00:21:17 +0530

    updated problems

--- src/com/test/hackerrank/stack/MaxRectangleInStack.java
+++ src/com/test/hackerrank/stack/MaxRectangleInStack.java
@@ -3,6 +3,36 @@ package com.test.hackerrank.stack;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * 
+ * There are buildings in a certain two-dimensional landscape. Each building has
+ * a height given by . If you join adjacent buildings, they will form a solid
+ * rectangle of area .
+ * 
+ * Given buildings, find the greatest such solid area formed by consecutive
+ * buildings.
+ * 
+ * Input Format
+ * 
+ * The first line contains , the number of buildings. The second line contains
+ * space-separated integers, each representing the height of a building.
+ * 
+ * Constraints
+ * 
+ * Output Format
+ * 
+ * Print a long integer representing the maximum area of rectangle formed.
+ * 
+ * Sample Input
+ * 
+ * 5 1 2 3 4 5 Sample Output
+ * 
+ * 9
+ * 
+ * 
+ * @author rajeevsingh
+ *
+ */
 public class MaxRectangleInStack {
 	static long largestRectangle(int[] h) {
 		// Complete this function
