e87675ff069f95ebfcee8923b140729190c74c40
Rajeev krishna Singh <srajeev@athenahealth.com>
2018-06-05 18:10:21 +0530

    binary tree lowest common ansector

---
+++ src/com/test/hackerrank/tree/TreeOperations.java
@@ -0,0 +1,15 @@
+package com.test.hackerrank.tree;
+
+public class TreeOperations {
+
+	static Node lca(Node root, int v1, int v2) {
+
+		if (root.data < v1 && root.data < v2) {
+			return lca(root.right, v1, v2);
+		} else if (root.data > v1 && root.data > v2) {
+			return lca(root.left, v1, v2);
+		} else {
+			return root;
+		}
+	}
+}
