b8661a1a048ffcbf2aa27877497d290995bbdbc3
Rajeev krishna Singh <srajeev@athenahealth.com>
2018-08-24 18:53:05 +0530

    Find cycle in undirected graph

--- src/com/test/graph/Graph.java
+++ src/com/test/graph/Graph.java
@@ -13,7 +13,7 @@ public class Graph {
 		V = v;
 		adj = new LinkedList[v];
 		for (int i = 0; i < v; ++i)
-			adj[i] = new LinkedList();
+			adj[i] = new LinkedList<Integer>();
 	}
 
 	// Function to add an edge into the graph
@@ -25,11 +25,14 @@ public class Graph {
 		Graph g = new Graph(4);
 
 		g.addEdge(0, 1);
+		g.addEdge(1, 0);
 		g.addEdge(0, 2);
-		g.addEdge(1, 2);
+//		g.addEdge(1, 2);
+//		g.addEdge(2, 1);
 		g.addEdge(2, 0);
 		g.addEdge(2, 3);
-		g.addEdge(3, 3);
+		g.addEdge(3, 2);
+//		g.addEdge(3, 3);
 		return g;
 	}
 
@@ -60,5 +63,36 @@ public class Graph {
 	public static void main(String[] args) {
 		Graph g = Graph.getSampleGraph();
 		g.topologicalSort();
+		System.out.println(g.isCyclic());
+	}
+
+	// Find cycle in undirected graph
+	public boolean isCyclic() {
+		boolean[] visited = new boolean[V];
+		for (int i = 0; i < V; i++) {
+			if (!visited[i]) {
+				if (isCyclicUtil(i, visited, -1)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean isCyclicUtil(int current, boolean[] visited, int parent) {
+		visited[current] = true;
+		Iterator<Integer> it = adj[current].iterator();
+		int i;
+		while (it.hasNext()) {
+			i = it.next();
+			if (!visited[i]) {
+				if (isCyclicUtil(i, visited, current)) {
+					return true;
+				}
+			} else if (i != parent) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
